656b3214774705f819328d20725df1986a354f10
SamDamilare <samuel.akhagbokhai@gmail.com>
2024-03-01 12:11:01 +0100

    worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Button } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/HomePage/Auth/AuthPage";
 import AuthPasswordPage from "./pages/HomePage/Auth/AuthPasswordPage";
 import CompleteAuthPage from "./pages/HomePage/Auth/CompleteAuthPage";
 import LoginPage from "./pages/HomePage/Auth/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import PicsDrawer from "./Components/Drawers/PicsDrawer";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/authpasswordpage" element={<AuthPasswordPage />} />
         <Route path="/completeauth" element={<CompleteAuthPage />} />
         <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/picsdrawer" element={<PicsDrawer />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
       </Routes>
     </>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "../../Components/Navigation/Footer";
+import Header from "../../Components/Navigation/Header";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      MyHome
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
