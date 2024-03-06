f7f3cf14ac5f6450e4cf1ace6cf306a40ebfa1f0
mareigh24 <ajayimary300@gmail.com>
2024-03-06 10:21:43 +0100

    woo

--- src/pages/HomePage/Auth/CompleteAuthPage.jsx
+++ src/pages/HomePage/Auth/CompleteAuthPage.jsx
@@ -1,28 +1,25 @@
+//
+
 import { Box, Image, Button, Container, VStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { auth, firestore } from "../../../Firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
+// import {auth} from "firebase/auth";
+import { firestore, auth } from "../../../Firebase/firebase";
+// import { auth, firestore } from "../../../Firebase/firebase"; // Import auth and firestore
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const CompleteAuthPage = () => {
   const navigate = useNavigate();
-  // const location = useLocation();
-
   const [createUserWithEmailAndPassword, error] =
     useCreateUserWithEmailAndPassword(auth);
 
-  // if (!inputs.email || inputs.password || inputs.username){
-  //   console.log("The process is not completed")
-  // }
-
   const handleSignup = async (inputs) => {
     navigate("/loginpage");
-    if (!inputs.email || inputs.username || inputs.password) {
-      console.log("ghe process is not completed");
+    if (!inputs.email || !inputs.username || !inputs.password) {
+      console.log("The process is not completed");
       return;
     }
     try {
-      // Create user with email and password
       const newUser = await createUserWithEmailAndPassword(
         inputs.email,
         inputs.password
@@ -33,7 +30,6 @@ const CompleteAuthPage = () => {
       }
 
       if (newUser) {
-        // Add user details to Firestore
         const userDoc = {
           uid: newUser.user.uid,
           email: inputs.email,
@@ -43,13 +39,11 @@ const CompleteAuthPage = () => {
           followers: [],
           following: [],
           posts: [],
-          createdAt: Date.now(), // Additional user data
+          createdAt: Date.now(),
         };
 
         await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
         localStorage.setItem("user-info", JSON.stringify(userDoc));
-
-        // Navigate to login page or dashboard after successful registration
       }
     } catch (error) {
       console.log(error);
@@ -67,9 +61,7 @@ const CompleteAuthPage = () => {
         <Box border={"1ps solid gray"} padding={5}>
           <VStack spacing={4}>
             <Image src="https://res.cloudinary.com/dktrwqio1/image/upload/v1708892258/picspile/Picspile_asn403.png" />
-
             <p>Welcome to picspile, Mareigh.</p>
-
             <Button onClick={() => handleSignup()}>Complete SignUp</Button>
             <hr />
             <p>English[United Kingdom]</p>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Container, Box } from "@chakra-ui/layout";
-import Avatars from "/Users/Latitude E5450/Desktop/picspile/src/Components/Avatars";
-import Feedposts from "/Users/Latitude E5450/Desktop/picspile/src/Components/Feedposts/Feedposts";
+import Avatars from "../../Components/Avatars";
+import Feedposts from "../../Components/Feedposts/Feedposts";
 import Homepage_header from "../../Components/Header/Homepage_header";
-
 
 const Homepage = () => {
   return (
